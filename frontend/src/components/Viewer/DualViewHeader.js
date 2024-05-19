6cb5e5e33b17910b86dfb3519ada9e0c9d37fd9d
Kay Han <kyungwanhan@gmail.com>
2024-05-19 02:44:46 -0400

    feat(frontend): Add link to navigate back to the first page - Add StyledLink component for header navigation

--- frontend/src/components/Viewer/DualViewHeader.js
+++ frontend/src/components/Viewer/DualViewHeader.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import StyledLink from './StyledLink';
 import WelcomeMessage from './WelcomeMessage';
 
 const SingleViewHeader = ({ userName, leftModelName, rightModelName }) => {
   return (
     <>
-      <h1>Web Inspector 2.0 </h1>
+      <h1>
+        <StyledLink to="/">Web Inspector 2.0</StyledLink>
+      </h1>{' '}
       User: {userName}, Left Model: {leftModelName}, Right Model:
       {rightModelName} <br />
       <WelcomeMessage />
