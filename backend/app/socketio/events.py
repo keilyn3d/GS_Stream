8ae59926c3abf8fb6904af946cd52d241b7397e3
Kay Han <kyungwanhan@gmail.com>
2024-06-08 05:29:18 -0400

    fix(backend): Consolidate pose values - Use a single pose value in dual view

--- backend/app/socketio/events.py
+++ backend/app/socketio/events.py
@@ -2,7 +2,7 @@ import base64
 from flask import request
 from flask_socketio import SocketIO, emit
 from ..image_renderer.image_creator import *
-from ..image_renderer.render_wrapper import decompose_44, compose_44
+from ..image_renderer.render_wrapper import decompose_44
 import logging
 import os
 from ..model_config.model_config_fetcher import ModelManager
@@ -56,19 +56,25 @@ def configure_socketio(socketio: SocketIO):
         emit('response', {'message': 'Pose reset'})
 
     @socketio.on('key_control')
-    def key_control(data):
+    def handle_key_control(data):
         keys, step = data['key'], data['step']
         print(f'Keys:{keys}, Step:{step} are received from User:{user_name}')
-        for model_id in model_ids:
-            current_pose = user_states[request.sid][model_id]['current_pose']
-            if ' ' in keys and keys[' ']:
-                handle_space_key(model_id, current_pose)
-            else:
-                pressed_keys = ''.join(key for key, pressed in keys.items() if pressed)
-                pressed_keys = pressed_keys[:2]  # Limit the length to 2
-                if pressed_keys:
-                    handle_other_keys(model_id, pressed_keys, step, current_pose)
-                    calculate_altitude(current_pose)
+        
+        def is_space_bar_pressed(keys):
+            return ' ' in keys and keys[' ']
+        
+        def get_pressed_keys(keys):
+            pressed_keys = ''.join(key for key, pressed in keys.items() if pressed)
+            pressed_keys = pressed_keys[:2]  # Limit the length to 2
+            return pressed_keys
+
+        if is_space_bar_pressed(keys):
+            handle_space_key()            
+        else:
+            pressed_keys = get_pressed_keys(keys)
+            if pressed_keys:
+                handle_other_keys(pressed_keys, step)
+            calculate_altitude()
                     
     @socketio.on('get_asset_pose')
     def handle_get_asset_pose(data):
@@ -112,48 +118,66 @@ def configure_socketio(socketio: SocketIO):
                     'current_pose': model.init_pose()
                 }
 
+def calculate_altitude():
+    # Select only one model for now
+    model_id = model_ids[0]
+    model = model_manager.get_model(model_id)
+    current_pose = user_states[request.sid][model_id]['current_pose']
+    R, T = decompose_44(np.array(current_pose))
+    altitude, heading = model.get_flight_params(R, T) 
+    logging.info(f'Altitude: {altitude} and Heading: {heading}')
+    emit("flight_params", {'altitude': altitude, 'heading': heading})
 
-def calculate_altitude(current_pose):
+def handle_space_key():
+    # Select only one model for now
+    model_id = model_ids[0]
+    current_pose = user_states[request.sid][model_id]['current_pose']
+    
+    # Get the closest images for the current pose of the model
     for model_id in model_ids:
         model = model_manager.get_model(model_id)
-        R, T = decompose_44(np.array(current_pose))
-        altitude, heading = model.get_flight_params(R, T) # how to process in dual modes?
-        logging.info(f'Altitude: {altitude} and Heading: {heading}')
-        emit("flight_params", {'altitude': altitude, 'heading': heading})
-
-def handle_space_key(model_id, current_pose):
-    model = model_manager.get_model(model_id)
-    filenames = model.images.get_closest_n(pose=current_pose, n=3)
-    print("The closest images are: " + ', '.join(str(x) for x in filenames))
-    closest_images = {}
-    filepath = model.images_thumbnails
-    for file in filenames:
-        with open(os.path.join(filepath, file), 'rb') as f:
-            img_data = f.read()
-            # Encode img_data to base64
-            closest_images[file] = base64.b64encode(img_data).decode('utf-8')
-    data = {
-        'modelId': model_id,
-        'images': closest_images
-    }
-    emit("nnImg", data)
+        filenames = model.images.get_closest_n(pose=current_pose, n=3)
+        print("The closest images are: " + ', '.join(str(x) for x in filenames))
+        closest_images = {}
+        filepath = model.images_thumbnails
+        for file in filenames:
+            with open(os.path.join(filepath, file), 'rb') as f:
+                img_data = f.read()
+                # Encode img_data to base64
+                closest_images[file] = base64.b64encode(img_data).decode('utf-8')
+        data = {
+            'modelId': model_id,
+            'images': closest_images
+        }
+        emit("nnImg", data)
 
 
-def handle_other_keys(model_id, keys, step, current_pose):
+def handle_other_keys(keys, step):
     print(f'Handle key input for {keys} from the client')
-    pose = current_pose
+    
+    # Select only one model for now
+    model_id = model_ids[0]
+    
+    # Get the current pose of the model
+    cam = None
+    pose = user_states[request.sid][model_id]['current_pose']       
+
+    # Change the cam and pose based on the key input
     for key in keys:
         cam = get_changed_cam(pose, key, step)
         pose = cam.get_new_pose()
-    img_data = model_manager.get_model(model_id).render_model_image(cam)  # Render and save the model image  
-    base64_img = make_base64_img(img_data)
-    user_states[request.sid][model_id]['current_pose'] = cam.get_new_pose()
-    print(f'Message to {user_name}: set_client_main_image')
-    data = {
-        'modelId': model_id,
-        'image': base64_img
-    }
-    emit('set_client_main_image', data)
+
+    # Render the model image with the new cam and pose
+    for model_id in model_ids:
+        img_data = model_manager.get_model(model_id).render_model_image(cam)  # Render and save the model image  
+        base64_img = make_base64_img(img_data)
+        user_states[request.sid][model_id]['current_pose'] = pose # Update the current pose
+        print(f'Message to {user_name}: set_client_main_image')
+        data = {
+            'modelId': model_id,
+            'image': base64_img
+        }
+        emit('set_client_main_image', data)
 
 def make_base64_img(image):
     image.seek(0)
