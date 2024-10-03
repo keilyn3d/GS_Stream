85a3a934197e78db31515a9fb3fa4ac108e42471
Kay Han <kyungwanhan@gmail.com>
2024-10-03 11:24:59 -0400

    feat(frontend): Add Docker configuration

---
+++ frontend/docker-build.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+set -e
+
+# Load environment variables from .env file (if available)
+if [ -f .env ]; then
+  export $(grep -v '^#' .env | xargs)
+fi
+
+# Set variables
+IMAGE_NAME="webinspector-csr-front"
+IMAGE_TAG="latest"
+FULL_TAG="$IMAGE_NAME:$IMAGE_TAG"
+
+echo "Starting Docker build for $IMAGE_NAME with REACT_APP_BACKEND_URL=$REACT_APP_BACKEND_URL and REACT_APP_CSR_BACKEND_URL=$REACT_APP_CSR_BACKEND_URL..."
+
+# Build and load Docker image locally
+docker build \
+  --build-arg REACT_APP_BACKEND_URL=$REACT_APP_BACKEND_URL \
+  --build-arg REACT_APP_CSR_BACKEND_URL=$REACT_APP_CSR_BACKEND_URL \
+  -t $FULL_TAG \
+  --progress=plain \
+  .
+
+
+echo "Docker image built and loaded to local Docker successfully: $FULL_TAG"
